b7817ac4ba2b7afd0f840ee0bd12f96ddac430e7
Aakif1997 <97013036+Aakif1997@users.noreply.github.com>
2022-07-20 15:36:53 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,32 @@
+package assignment ;
+
+import java.util.Scanner;
+
+public class calculation {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter the number");
+	double a=scan.nextDouble();
+	double b=scan.nextDouble();
+	double c=a+b;
+	double d=a-b;
+	double e=a*b;
+	double f=a%b;
+	
+	System.out.println("the sum is "+c);
+	System.out.println("he sum is " +d);
+	System.out.println("the sum is "+e);
+	System.out.println("the sum is "+f);
+	
+}
+
+private static void Scanner(String string) {
+	// TODO Auto-generated method stub
+	
+}
+
+private static void println(String string) {
+	// TODO Auto-generated method stub
+	
+}
+}
