ee3180ed831d46a4f1e76825712517078aff6537
Aakif1997 <97013036+Aakif1997@users.noreply.github.com>
2022-07-20 14:59:16 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,36 @@
+
+public class switchcase {
+
+	public static void main (String[] args) {
+	
+	String days="tuesday"
+	
+	Switch (days); 
+	
+		case"monday";
+		System.out.println(01);
+		break;
+		case"tuesday";
+		System.out.println(02);
+		break;
+		case"wednesday";
+		System.out.println(03);
+		break;
+		case"thurseday";
+		System.out.println(04);
+		break;
+		case"friday";
+		System.out.println(05);
+		break
+		case"saturday";
+		System.out.println(06);
+		break;
+		case"sunday";
+		System.out.println(07);
+        break;
+	}
+	}
+}
+
+	
+	
