fbb8e6988bf6ede1a3af213be4379ea68d838429
Aakif1997 <97013036+Aakif1997@users.noreply.github.com>
2022-07-20 14:37:38 +0530

    Add files via upload

---
+++ ArrayClass.java
@@ -0,0 +1,30 @@
+package com.besant.control;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		//int[] number=new int[] {1,2,3,4,5};
+		int[] numbers= new int[5];
+		//int n;
+		//[99,5,7,10,82]  -> 99 -(0) , 5 - (1) , .., 82-(4) n-1
+		//ArrayOutofBoundException
+		//int num[];
+		numbers[0]=1;
+		//numbers[2]=45;
+		numbers[3]=76;
+		numbers[4]=98;
+		numbers[1]=83;
+		
+		System.out.println(numbers[2]);
+		
+	int[] newIndex= {1,4,5,6};
+		Object numb=1;
+		Object word="Hello";
+		
+		Object[] objectArr= {1,"Hi",true};
+		
+	
+		
+	}
+
+}
