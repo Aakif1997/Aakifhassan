c05471c34a07658e69d663ab739a245e29aa8026
Aakif1997 <97013036+Aakif1997@users.noreply.github.com>
2022-07-21 11:39:18 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,21 @@
+
+public class pattern5 {
+ public static void main(String[] args) {
+	int n=4;
+	for (int i=1;i<n;i++) {
+		System.out.print(" ");
+	}
+	System.out.println();
+ 
+    n=n+1;
+    for(int i=1;i<=n;i++) {
+    	for(int j=4;j>=1;j--) {
+    		System.out.print(" * ");
+    	}
+    	System.out.println();
+    }
+    	}
+}
+  
+    
+    
