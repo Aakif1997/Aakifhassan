d2d0cc6c7b05bcf5a9875827a932b1021794e548
Aakif1997 <97013036+Aakif1997@users.noreply.github.com>
2022-07-21 11:36:54 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,20 @@
+
+public class Loops {
+	public static void main(String[] args) {
+		int i=0;
+		while(i<10) {
+		
+		  i++;++i;i=i+1;
+		 
+			i=i+3;
+			System.out.println((i-1)+""+(i-2)+""+i);
+		}
+		for(int x=10;x>=1;x--) {
+			System.out.println(""+x);
+		}
+					
+					
+					
+
+		}
+	}
